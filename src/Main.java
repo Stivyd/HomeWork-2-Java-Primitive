203dca0bd50820f216347f4c8c4f11cd18057826
Dmitry Teterin Qamid-61 <Teterin130215@yandex.ru>
2023-04-16 13:42:01 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 65_485;
+        int percent = 20;
+        int bonus = amount / percent;
+
+        System.out.println("Your bonus is:"+ bonus +" miles");
+
+
+    }
+}
